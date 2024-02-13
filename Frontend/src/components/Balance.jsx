924b3322f7bbdb33c1adefbb5744cbe20fcfb947
hemraj bhatia <hemrajbhatia38@gmail.com>
2024-02-14 00:10:19 +0530

    paytm

---
+++ Frontend/src/components/Balance.jsx
@@ -0,0 +1,8 @@
+export const Balance=({value})=>{
+    return <div className="flex">
+        <div className="font-semibold text-lg">Your Balance</div>
+        <div className="font-semibold ml-4 text-lg">
+            Rs {value}
+        </div>
+    </div>
+}
---
+++ Frontend/src/components/Heading.jsx
@@ -0,0 +1,5 @@
+export function Heading({label}){
+    return <div className="font-bold test-4xl pt-6">
+        {label}
+    </div>
+}
